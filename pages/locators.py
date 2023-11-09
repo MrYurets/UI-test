068ba2f1770d3d26afa4f1c242d66f8dc9cb3f6d
MrYurets <ura9464@mail.ru>
2023-11-09 12:49:10 +0300

    Добавил регистрацию нового пользователя, и тесты, и тесты, что он может добавлять товары в корзину

--- pages/locators.py
+++ pages/locators.py
@@ -1,11 +1,15 @@
 from selenium.webdriver.common.by import By
 
-class LoginPageLocators():
-    LOGINFORM_LINK = (By.CSS_SELECTOR, "#login_form")
-    REGISTERFORM_LINK = (By.CSS_SELECTOR, "#register_form")
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user") # Иконка, которая появляется при успешной регистрации
 
 class ProductPageLocators():
-    BASKET_LINK = (By.CLASS_NAME, "btn-add-to-basket") # Селектор кнопки добавления  в корзину
+    BASKET_LINK = (By.CLASS_NAME, "btn-add-to-basket") # Селектор кнопки добавления в корзину
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div.alertinner") # Селектор сообщения о добавлении книги в корзину
     NAME_BOOK_BASKET = (By.CSS_SELECTOR, ".alertinner strong") # Название добавленной книги
     PRICE_BOOK_BASKET = (By.CSS_SELECTOR, ".alertinner p strong") # Цена добавленной книги
@@ -14,9 +18,14 @@ class ProductPageLocators():
     BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-group a.btn-default ") # Кнопка добавления в корзину
 
 class BasketPageLocators():
-    BASKET_CONTENT = (By.CSS_SELECTOR, "#content_inner p") # Сообщение о том что  корзина пуста
-    BASKET_PRODUCT = (By.CSS_SELECTOR, "div.basket-title div.row") # Товары которые есть в корзине
+    BASKET_CONTENT = (By.CSS_SELECTOR, "#content_inner p") # Сообщение о том что корзина пуста
+    BASKET_PRODUCT = (By.CSS_SELECTOR, "div.basket-title div.row") # Товары, которые есть в корзине
 
-class BasePageLocators():
+class LoginPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    LOGINFORM_LINK = (By.CSS_SELECTOR, "#login_form")
+    REGISTERFORM_LINK = (By.CSS_SELECTOR, "#register_form")
+    EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    CONFIRM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_BUTTON = (By.NAME, "registration_submit")
--- test_product_page.py
+++ test_product_page.py
@@ -1,61 +1,90 @@
 from .pages.basket_page import BasketPage
 from .pages.product_page import ProductPage
+from pages.login_page import LoginPage
 import time
 import pytest
 
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_can_add_product_to_basket(browser):
-    """Тестируем, что гость может добавлять товары в корзмину"""
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_basket_form()
-    page.add_item_in_basket()
-    page.solve_quiz_and_get_code()
-    page.conditions_when_adding_to_cart()
-    time.sleep(5)
+class TestUserAddToBasketFromProductPage():
+    """Создаем класс, чтобы тестировать от лица зарегистрированного пользователя """
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        login_link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+        page = LoginPage(browser, login_link)
+        page.open()
+        email = str(time.time()) + "@fakemail.org" # Делаем новый email
+        password = str(time.time()) + "123" # Делаем новый пароль
+        page.register_new_user(email, password) # Регистрируем нового пользователя
+        page.should_be_authorized_user() # Проверяем что пользователь авторизован
+
+    def test_user_cant_see_success_message(self, browser):
+        """Тестируем, что пользователю не показывается сообщение об успешном добавлении товара пока он его не добавит"""
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        """Тестируем, что пользователь может добавлять товары в корзину"""
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_basket_form()
+        page.add_item_in_basket()
+        # page.solve_quiz_and_get_code() # Закомментировал, потому что на новой ссылки нет капчи, и её делать не нужно
+        page.conditions_when_adding_to_cart()
+
+class TestGuestAddToBasketFromProductPage():
+    def test_guest_can_add_product_to_basket(browser):
+        """Тестируем, что гость может добавлять товары в корзину"""
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_basket_form()
+        page.add_item_in_basket()
+        page.solve_quiz_and_get_code()
+        page.conditions_when_adding_to_cart()
+        time.sleep(5)
 
 
-@pytest.mark.skip
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    """Тестируем,что гость не видит сообщение о добавлении товарав корзину"""
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_item_in_basket()
-    page.should_not_be_success_message()
+    @pytest.mark.skip
+    def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+        """Тестируем, что гость не видит сообщение о добавлении товара в корзину"""
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_item_in_basket()
+        page.should_not_be_success_message()
 
-def test_guest_cant_see_success_message(browser):
-     """Тестируем, что гость не показывается сообщение об успешном добавлении товара пока он не нажимает добавить"""
-     page = ProductPage(browser, link)
-     page.open()
-     page.should_not_be_success_message()
+    def test_guest_cant_see_success_message(browser):
+         """Тестируем, что гость не показывается сообщение об успешном добавлении товара пока он не нажимает добавить"""
+         page = ProductPage(browser, link)
+         page.open()
+         page.should_not_be_success_message()
 
 
-@pytest.mark.xfail
-def test_message_disappeared_after_adding_product_to_basket(browser):
-    """Тестируем,что сообщение о добавления в корзину исчезает после нажатия"""
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_item_in_basket()
-    page.is_disappeared()
+    @pytest.mark.xfail
+    def test_message_disappeared_after_adding_product_to_basket(browser):
+        """Тестируем, что сообщение о добавления в корзину исчезает после нажатия"""
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_item_in_basket()
+        page.is_disappeared()
 
-def test_guest_should_see_login_link_on_product_page(browser):
-    """Тестируем что гость должен увидеть ссылку на рагистрациию настранице"""
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link_on_product_page(browser):
+        """Тестируем что гость должен увидеть ссылку на регистрацию на странице"""
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    """Тестирует что гость может нажать на ссылку регистрациир на этой странице"""
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_login_page()
+    def test_guest_can_go_to_login_page_from_product_page(browser):
+        """Тестирует что гость может нажать на ссылку регистрацию на этой странице"""
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_login_page()
 
-def  test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    """Тестируем что гость переходит в корзину и проверяет что в корзине пусто"""
-    page = BasketPage(browser, link)
-    page.open()
-    page.go_to_basket_page()
-    page.should_not_be_product_basket()
-    page.should_be_empty_basket()
+    def  test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+        """Тестируем что гость переходит в корзину и проверяет что в корзине пусто"""
+        page = BasketPage(browser, link)
+        page.open()
+        page.go_to_basket_page()
+        page.should_not_be_product_basket()
+        page.should_be_empty_basket()
